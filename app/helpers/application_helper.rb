52fb8f801e4d323b0c038d610ed37b6f5692bb3d
benjamin4ruby <benjamin@railsvorlesung.com>
2010-06-22 16:54:46 +0200

    Finish translation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def link_language(locale, label)
+    html_options = { :class => ["lang_#{locale} "]}
+    html_options[:class] << 'active ' if locale_active? locale
+    
+    link_to label, { :locale => locale }, html_options
+  end
+  
+  def locale_active?(locale)
+    I18n.locale == locale
+  end
 end
